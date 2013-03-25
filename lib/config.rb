3d617e51db016271fae69cb0473060f9172dd233
Alexey Noskov <alexey.noskov@gmail.com>
2013-03-25 23:19:11 +0400

    Explicit yaml require in config

--- lib/config.rb
+++ lib/config.rb
@@ -1,3 +1,4 @@
+require 'yaml'
 
 APP_CONFIG_FILE = File.open(File.expand_path('../config/application.yml', File.dirname(__FILE__)))
 APP_CONFIG = YAML.load(APP_CONFIG_FILE)[ENV['RACK_ENV'] || ENV['RAILS_ENV'] || 'development']
