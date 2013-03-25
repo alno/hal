b243919efe3298347dfb02fd09a2d4f1496de43b
Alexey Noskov <alexey.noskov@gmail.com>
2013-03-25 23:03:57 +0400

    Gauge values table

---
+++ db/migrate/002_setup_gauge_values.rb
@@ -0,0 +1,14 @@
+Sequel.migration do
+
+  change do
+    create_table :gauge_values do
+      column :gauge, 'integer', null: false
+      column :time, 'timestamp', null: false
+
+      column :value, 'real', null: false
+
+      primary_key [ :gauge, :time ]
+    end
+  end
+
+end
