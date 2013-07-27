d9450b8ccb86f5ef39dfc0c62bdcb6b95ccb6d13
Alexey Noskov <alexey.noskov@gmail.com>
2013-07-28 00:10:45 +0400

    Selecting data table based on range

--- lib/gauge.rb
+++ lib/gauge.rb
@@ -21,12 +21,26 @@ Gauge = Struct.new :id, :index, :name, :source do
     DB[:gauge_values].insert gauge: index, time: Time.now, value: val
   end
 
-  def values
-    DB[:gauge_values].where(gauge: index)
+  def values_table(from = nil, to = nil)
+    if from.nil? || to.nil?
+      :gauge_value_hours
+    elsif to.to_i - from.to_i < 10 * 24 * 60 * 1000 # Less than 10 days - by minute
+      :gauge_values
+    elsif to.to_i - from.to_i < 600 * 24 * 60 * 1000 # Less than 600 days - by hour
+      :gauge_value_hours
+    else
+      :gauge_value_days
+    end
   end
 
-  def values_as_json
-    values.select(:time, :value).map{|r| [r[:time].to_i * 1000, r[:value]]}
+  def values(from = nil, to = nil)
+    from ||= DB[:gauge_values].min(:time)
+    to   ||= DB[:gauge_values].max(:time)
+    DB[values_table(from, to)].where('gauge = ? AND time BETWEEN ? AND ?', index, from, to)
+  end
+
+  def values_as_json(from = nil, to = nil)
+    values(from, to).select(:time, :value).map{|r| [r[:time].to_i * 1000, r[:value]]}
   end
 
 end
