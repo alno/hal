ead7fa1a7899037a165f7abf7800743e82bbdad6
Alexey Noskov <alexey.noskov@gmail.com>
2016-03-06 15:07:12 +0300

    Full module declaration in event bus

--- lib/hal/event_bus.rb
+++ lib/hal/event_bus.rb
@@ -1,21 +1,21 @@
-module Hal; end
+module Hal
+  class EventBus
 
-class Hal::EventBus
+    def initialize
+      @subscriptions = Hash.new{ |h, k| h[k] = [] }
+    end
 
-  def initialize
-    @subscriptions = Hash.new{ |h, k| h[k] = [] }
+    def publish(topic, event)
+      @subscriptions[topic].each{ |s| s.call(event) }
+    end
+
+    def subscribe(topic, handler)
+      @subscriptions[topic] << handler unless @subscriptions[topic].include? handler
+    end
+
+    def unsubscribe(topic, handler)
+      @subscriptions[topic].delete(handler)
+    end
+
   end
-
-  def publish(topic, event)
-    @subscriptions[topic].each{ |s| s.call(event) }
-  end
-
-  def subscribe(topic, handler)
-    @subscriptions[topic] << handler unless @subscriptions[topic].include? handler
-  end
-
-  def unsubscribe(topic, handler)
-    @subscriptions[topic].delete(handler)
-  end
-
 end
