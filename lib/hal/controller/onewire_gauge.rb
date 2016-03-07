c0f22c1c8dc9c2b2c039afb0202b82eafa2d40f8
Alexey Noskov <alexey.noskov@gmail.com>
2016-03-07 19:36:27 +0300

    Check value avaliability, more logging

--- lib/hal/controller/onewire_gauge.rb
+++ lib/hal/controller/onewire_gauge.rb
@@ -23,11 +23,15 @@ class Hal::Controller::OnewireGauge < Hal::Controller::Base
   end
 
   def update
-    puts "Updating gauge #{options[:path].inspect} in #{Time.now}"
+    puts "Updating gauge #{node.path} from #{options[:path].inspect} in #{Time.now}"
 
-    bus.publish node.path, @client.read(options[:path])
+    if value = @client.read(options[:path])
+      bus.publish node.path, value
+    else
+      puts "No value avaliable for #{node.path}"
+    end
   rescue => e
-    puts "Error updating: #{e.message}"
+    puts "Error updating #{node.path}: #{e.message}"
   end
 
 end
