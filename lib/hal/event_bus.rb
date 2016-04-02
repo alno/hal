888ebf4359017c25b4e089c6ca2cdce7b04a484a
Alexey Noskov <alexey.noskov@gmail.com>
2016-04-02 20:18:51 +0300

    EventBus support for paths

--- lib/hal/event_bus.rb
+++ lib/hal/event_bus.rb
@@ -5,16 +5,22 @@ class Hal::EventBus
   end
 
   def publish(topic, event)
+    topic = topic.to_s
+
     puts "Publishing #{event.inspect} in #{topic.inspect} topic"
 
     @subscriptions[topic].each { |s| s.call(event) }
   end
 
   def subscribe(topic, handler)
+    topic = topic.to_s
+
     @subscriptions[topic] << handler unless @subscriptions[topic].include? handler
   end
 
   def unsubscribe(topic, handler)
+    topic = topic.to_s
+
     @subscriptions[topic].delete(handler)
   end
 
--- spec/event_bus_spec.rb
+++ spec/event_bus_spec.rb
@@ -20,6 +20,12 @@ describe Hal::EventBus do
         bus.publish('some-topic', 'some-event')
       end
 
+      it "receives events in correct topic if registered as path" do
+        expect(subject).to receive(:call).with('some-event')
+
+        bus.publish(Hal::Path['some-topic'], 'some-event')
+      end
+
       it "doesn't receive events in wrong topic" do
         expect(subject).not_to receive(:call).with('some-event')
 
@@ -38,6 +44,33 @@ describe Hal::EventBus do
 
         bus.subscribe('some-topic', subject.method(:call))
         bus.publish('some-topic', 'some-event')
+      end
+
+    end
+
+
+    context "after calling #subscribe with path" do
+
+      before do
+        bus.subscribe(Hal::Path.root / 'fff' / 'ggg', subject.method(:call))
+      end
+
+      it "receives events in correct topic" do
+        expect(subject).to receive(:call).with('some-event')
+
+        bus.publish('/fff/ggg', 'some-event')
+      end
+
+      it "receives events in correct topic if registered as path" do
+        expect(subject).to receive(:call).with('some-event')
+
+        bus.publish(Hal::Path['/fff/ggg'], 'some-event')
+      end
+
+      it "doesn't receive events in wrong topic" do
+        expect(subject).not_to receive(:call).with('some-event')
+
+        bus.publish('fff/ggg', 'some-event')
       end
 
     end
