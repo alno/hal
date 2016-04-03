2abb54eb7903711b53c497a564876427dda26762
Alexey Noskov <alexey.noskov@gmail.com>
2016-04-03 23:37:20 +0300

    Fix value updating

--- lib/hal/persistor/contact.rb
+++ lib/hal/persistor/contact.rb
@@ -5,7 +5,7 @@ class Hal::Persistor::Contact < Hal::Persistor::Base
   private
 
   def update(val)
-    DB[:state_changes].insert node: node.path, time: Time.now, value: MultiJson.encode(val)
+    DB[:state_changes].insert node: node.path.to_s, time: Time.now, value: MultiJson.encode(val)
   end
 
 end
--- lib/hal/persistor/gauge.rb
+++ lib/hal/persistor/gauge.rb
@@ -5,7 +5,7 @@ class Hal::Persistor::Gauge < Hal::Persistor::Base
   private
 
   def update(val)
-    DB[:gauge_values].insert gauge: node.path, time: Time.now, value: val
+    DB[:gauge_values].insert gauge: node.path.to_s, time: Time.now, value: val
   end
 
 end
